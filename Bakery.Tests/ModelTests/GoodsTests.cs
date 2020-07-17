408426cb86c2bff37b577ace88c675332558beab
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-17 12:52:52 -0700

    meaninful fail - test two (2) item bread order

--- Bakery.Tests/ModelTests/GoodsTests.cs
+++ Bakery.Tests/ModelTests/GoodsTests.cs
@@ -8,19 +8,27 @@ namespace Bakery.Tests
   public class BakeryTests
   {
 
-    // [TestMethod]
-    // public void Bakery_GetPrice_BreadPrice()
-    // {
-    //   Bread tester = new Bread();
-    //   Assert.AreEqual(typeof(Bread), tester.GetType());
-    // }
-
-
     [TestMethod]
     public void Bakery_GetPrice_BreadPrice()
     {
+      Bread tester = new Bread();
+      Assert.AreEqual(typeof(Bread), tester.GetType());
+    }
+
+
+    [TestMethod]
+    public void Bakery_GetPrice_BreadPrice1()
+    {
     Bread tester = new Bread();
-    int price = tester.GetPrice("one");
+    int price = tester.GetPrice(1);
+    Assert.AreEqual(5, price);
+    }
+
+    [TestMethod]
+    public void Bakery_GetPrice_BreadPrice2()
+    {
+    Bread tester = new Bread();
+    int price = tester.GetPrice(2);
     Assert.AreEqual(5, price);
     }
 
